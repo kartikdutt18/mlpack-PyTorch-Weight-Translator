2a9b602a9f953e391da20c23b7b5c64f6ab9dda2
kartikdutt18 <kartikdutt@live.in>
2020-08-11 11:23:43 +0530

    Changes in run file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 weight_extractor.py darknet19
-cd mlpack-loader && git clone --single-branch --branch BranchForConverter https://github.com/kartikdutt18/models.git
+cd mlpack-loader && git clone https://github.com/kartikdutt18/models.git
 cd models && mkdir build 
 cd build && cmake ../ && sudo make -j2 && ./bin/imagenette_trainer
